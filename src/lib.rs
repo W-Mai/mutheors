1eeee4b1b71de6a2f7b67804e9cb50f802a0ff6b
Benign X <1341398182@qq.com>
2025-03-10 04:32:34 +0800

    ✨(README): update project description
    The README now includes a clear overview of the MuTheoRS library, detailing its features and modules. This provides users with essential information to understand the available tools for music theory concepts at a glance.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,20 @@
+//! # MuTheoRS
+//! > This crate provides a set of tools for working with music theory concepts,
+//!
+//! - PitchClass: C/D/E/F/G/A/B...
+//! - Tuning: C4/C#4/D4/E4/F4/G4/A4/B4...
+//! - Duration: quarter, eighth, half...
+//! - Note: C4 quarter, C4 eighth, C4 half...
+//! - Chord: C major, C minor, C7...
+//! - Measure: bundle of notes and chords
+//! - Track: bundle of measures
+//! - Score: bundle of tracks
+//!
+//! - Midi: play the score using midi
+//!
+//! Other Abilities:
+//! - Interval: describe the distance between two `Tuning`s
+
 #[cfg(feature = "midi_io")]
 mod midi;
 #[cfg(feature = "midi_io")]
