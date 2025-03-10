9cf9a46a1a60ee6ef268137e700e5da5375aaca0
Benign X <1341398182@qq.com>
2025-03-11 00:31:45 +0800

    ✨(measure): implement Display for Measure enum
    This adds the Display trait implementation for the Measure enum, enabling formatted output.
    
    Now, users can easily print Measure instances, with specific formats for Rest, Chord, and Note types. This enhances usability and clarity when working with musical measures in the code.

--- src/composition/measure.rs
+++ src/composition/measure.rs
@@ -1,4 +1,6 @@
 use crate::{Chord, Note};
+use std::fmt::Display;
+use std::ptr::write;
 
 #[derive(Clone)]
 pub enum Measure {
@@ -22,5 +24,18 @@ impl Measure {
 
     pub fn note(&mut self, notes: Vec<Note>) {
         *self = Self::Note(notes);
+    }
+}
+
+impl Display for Measure {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Measure::Rest => write!(f, "{}", "Rest"),
+            Measure::Chord(chord) => write!(f, "{}{}", chord.root(), chord.quality()),
+            Measure::Note(notes) => {
+                let notes_str: Vec<String> = notes.iter().map(|n| n.to_string()).collect();
+                write!(f, "{}", notes_str.join(" "))
+            }
+        }
     }
 }
