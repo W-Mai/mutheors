aa4734c0a872a9371fdb07de1430b46b6dc9da99
Benign X <1341398182@qq.com>
2025-03-13 02:20:26 +0800

    ✨(test): add test for two tigers score with multiple measures and chords

--- tests/test_midi.rs
+++ tests/test_midi.rs
@@ -104,4 +104,96 @@ mod tests {
 
         midi_player.close();
     }
+
+    #[test]
+    fn test_two_tigers() {
+        let mut score = Score::<2>::new()
+            .with_tempo(120.0)
+            .with_time_signature(4, DurationBase::Quarter);
+
+        let dg = score.duration_generator();
+
+        score.new_measures(|m| {
+            m[0].chord(Chord::triad(tuning!(C 4), ChordQuality::Major).unwrap());
+            m[1].note(beats!(dg;
+                1.0 => tuning!(C 4),
+                1.0 => tuning!(D 4),
+                1.0 => tuning!(E 4),
+                1.0 => tuning!(C 4),
+            ));
+        });
+
+        score.new_measures(|m| {
+            m[0].chord(Chord::triad(tuning!(C 4), ChordQuality::Major).unwrap());
+            m[1].note(beats!(dg;
+                1.0 => tuning!(C 4),
+                1.0 => tuning!(D 4),
+                1.0 => tuning!(E 4),
+                1.0 => tuning!(C 4),
+            ));
+        });
+
+        score.new_measures(|m| {
+            m[0].chord(Chord::triad(tuning!(G 4), ChordQuality::Major).unwrap());
+            m[1].note(beats!(dg;
+                1.0 => tuning!(E 4),
+                1.0 => tuning!(F 4),
+                2.0 => tuning!(G 4),
+            ));
+        });
+
+        score.new_measures(|m| {
+            m[0].chord(Chord::triad(tuning!(G 4), ChordQuality::Major).unwrap());
+            m[1].note(beats!(dg;
+                1.0 => tuning!(E 4),
+                1.0 => tuning!(F 4),
+                2.0 => tuning!(G 4),
+            ));
+        });
+
+        score.new_measures(|m| {
+            m[0].chord(Chord::triad(tuning!(A 4), ChordQuality::Minor).unwrap());
+            m[1].note(beats!(dg;
+                0.5 => tuning!(G 4),
+                0.5 => tuning!(A 4),
+                0.5 => tuning!(G 4),
+                0.5 => tuning!(F 4),
+                1.0 => tuning!(E 4),
+                1.0 => tuning!(C 4),
+            ));
+        });
+
+        score.new_measures(|m| {
+            m[0].chord(Chord::triad(tuning!(A 4), ChordQuality::Minor).unwrap());
+            m[1].note(beats!(dg;
+                0.5 => tuning!(G 4),
+                0.5 => tuning!(A 4),
+                0.5 => tuning!(G 4),
+                0.5 => tuning!(F 4),
+                1.0 => tuning!(E 4),
+                1.0 => tuning!(C 4),
+            ));
+        });
+
+        score.new_measures(|m| {
+            m[0].chord(Chord::triad(tuning!(E 4), ChordQuality::Minor).unwrap());
+            m[1].note(beats!(dg;
+                1.0 => tuning!(E 4),
+                1.0 => tuning!(G 3),
+                2.0 => tuning!(C 4),
+            ));
+        });
+
+        score.new_measures(|m| {
+            m[0].chord(Chord::triad(tuning!(E 4), ChordQuality::Minor).unwrap());
+            m[1].note(beats!(dg;
+                1.0 => tuning!(E 4),
+                1.0 => tuning!(G 3),
+                2.0 => tuning!(C 4),
+            ));
+        });
+
+        let mut midi_player = MidiPlayer::new("Simple Compose");
+        midi_player.play_score(score).unwrap();
+    }
 }
