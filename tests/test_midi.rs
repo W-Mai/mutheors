e724171dbfdc1fed61d8d6d5249e289be8db8c1b
Benign X <1341398182@qq.com>
2025-03-30 22:10:19 +0800

    💖(tests): update tempo and duration in test cases
    Refactor test cases to change the tempo from Vivace to Andante and modify the time signature from 4/4 to 2/4, resulting in a more relaxed pacing. Additionally, adjust the note durations to create a more flowing rhythm throughout the measures.

--- tests/test_midi.rs
+++ tests/test_midi.rs
@@ -143,69 +143,78 @@ mod tests {
     #[test]
     fn test_two_tigers() {
         let mut score = Score::<2>::new()
-            .with_tempo(Tempo::Vivace)
-            .with_time_signature(4, DurationBase::Quarter);
+            .with_tempo(Tempo::Andante)
+            .with_time_signature(2, DurationBase::Quarter);
 
         let dg = score.duration_generator();
 
         score.new_measures(|m| {
             m[0].chord(Chord::triad(tuning!(C 4), ChordQuality::Major).unwrap());
             m[1].note(beats!(dg;
-                1.0 => tuning!(C 4),
-                1.0 => tuning!(D 4),
-                1.0 => tuning!(E 4),
-                1.0 => tuning!(C 4),
+                0.5 => tuning!(C 4),
+                0.5 => tuning!(D 4),
+                0.5 => tuning!(E 4),
+                0.5 => tuning!(C 4),
             ));
         });
 
         score.new_measures(|m| {
             m[0].chord(Chord::triad(tuning!(C 4), ChordQuality::Major).unwrap());
             m[1].note(beats!(dg;
-                1.0 => tuning!(C 4),
-                1.0 => tuning!(D 4),
-                1.0 => tuning!(E 4),
-                1.0 => tuning!(C 4),
+                0.5 => tuning!(C 4),
+                0.5 => tuning!(D 4),
+                0.5 => tuning!(E 4),
+                0.5 => tuning!(C 4),
             ));
         });
 
         score.new_measures(|m| {
             m[0].chord(Chord::triad(tuning!(G 4), ChordQuality::Major).unwrap());
             m[1].note(beats!(dg;
-                1.0 => tuning!(E 4),
-                1.0 => tuning!(F 4),
-                2.0 => tuning!(G 4),
+                0.5 => tuning!(E 4),
+                0.5 => tuning!(F 4),
+                1.0 => tuning!(G 4),
             ));
         });
 
         score.new_measures(|m| {
             m[0].chord(Chord::triad(tuning!(G 4), ChordQuality::Major).unwrap());
             m[1].note(beats!(dg;
-                1.0 => tuning!(E 4),
-                1.0 => tuning!(F 4),
-                2.0 => tuning!(G 4),
+                0.5 => tuning!(E 4),
+                0.5 => tuning!(F 4),
+                1.0 => tuning!(G 4),
             ));
         });
 
         score.new_measures(|m| {
             m[0].chord(Chord::triad(tuning!(A 4), ChordQuality::Minor).unwrap());
             m[1].note(beats!(dg;
-                0.5 => tuning!(G 4),
-                0.5 => tuning!(A 4),
-                0.5 => tuning!(G 4),
-                0.5 => tuning!(F 4),
-                1.0 => tuning!(E 4),
-                1.0 => tuning!(C 4),
+                0.25 => tuning!(G 4),
+                0.25 => tuning!(A 4),
+                0.25 => tuning!(G 4),
+                0.25 => tuning!(F 4),
+                0.5 => tuning!(E 4),
+                0.5 => tuning!(C 4),
             ));
         });
 
         score.new_measures(|m| {
             m[0].chord(Chord::triad(tuning!(A 4), ChordQuality::Minor).unwrap());
             m[1].note(beats!(dg;
-                0.5 => tuning!(G 4),
-                0.5 => tuning!(A 4),
-                0.5 => tuning!(G 4),
-                0.5 => tuning!(F 4),
-                1.0 => tuning!(E 4),
+                0.25 => tuning!(G 4),
+                0.25 => tuning!(A 4),
+                0.25 => tuning!(G 4),
+                0.25 => tuning!(F 4),
+                0.5 => tuning!(E 4),
+                0.5 => tuning!(C 4),
+            ));
+        });
+
+        score.new_measures(|m| {
+            m[0].chord(Chord::triad(tuning!(E 4), ChordQuality::Minor).unwrap());
+            m[1].note(beats!(dg;
+                0.5 => tuning!(E 4),
+                0.5 => tuning!(G 3),
                 1.0 => tuning!(C 4),
             ));
         });
@@ -213,18 +222,9 @@ mod tests {
         score.new_measures(|m| {
             m[0].chord(Chord::triad(tuning!(E 4), ChordQuality::Minor).unwrap());
             m[1].note(beats!(dg;
-                1.0 => tuning!(E 4),
-                1.0 => tuning!(G 3),
-                2.0 => tuning!(C 4),
-            ));
-        });
-
-        score.new_measures(|m| {
-            m[0].chord(Chord::triad(tuning!(E 4), ChordQuality::Minor).unwrap());
-            m[1].note(beats!(dg;
-                1.0 => tuning!(E 4),
-                1.0 => tuning!(G 3),
-                2.0 => tuning!(C 4),
+                0.5 => tuning!(E 4),
+                0.5 => tuning!(G 3),
+                1.0 => tuning!(C 4),
             ));
         });
 
@@ -234,8 +234,8 @@ mod tests {
     #[test]
     fn test_two_tigers_with_diff_scale_type() {
         let mut score = Score::<2>::new()
-            .with_tempo(Tempo::Vivace)
-            .with_time_signature(4, DurationBase::Quarter);
+            .with_tempo(Tempo::Andante)
+            .with_time_signature(2, DurationBase::Quarter);
 
         let scale = Scale::new(tuning!(C 4), ScaleType::Hirajoshi).unwrap();
         let dg = score.duration_generator();
@@ -244,10 +244,10 @@ mod tests {
             score.new_measures(|m| {
                 m[0].chord(Chord::seventh(tuning!(C 4), ChordQuality::Dominant7).unwrap());
                 m[1].note(beats!(dg;
-                    1.0 => scale(1),
-                    1.0 => scale(2),
-                    1.0 => scale(3),
-                    1.0 => scale(1),
+                    0.5 => scale(1),
+                    0.5 => scale(2),
+                    0.5 => scale(3),
+                    0.5 => scale(1),
                 ));
             });
         });
@@ -256,9 +256,9 @@ mod tests {
             score.new_measures(|m| {
                 m[0].chord(Chord::triad(tuning!(G 4), ChordQuality::Diminished).unwrap());
                 m[1].note(beats!(dg;
-                    1.0 => scale(3),
-                    1.0 => scale(4),
-                    2.0 => scale(5),
+                    0.5 => scale(3),
+                    0.5 => scale(4),
+                    1.0 => scale(5),
                 ));
             });
         });
@@ -267,12 +267,12 @@ mod tests {
             score.new_measures(|m| {
                 m[0].chord(Chord::seventh(tuning!(A 4), ChordQuality::FullyDiminished).unwrap());
                 m[1].note(beats!(dg;
-                    0.5 => scale(5),
-                    0.5 => scale(6),
-                    0.5 => scale(5),
-                    0.5 => scale(4),
-                    1.0 => scale(3),
-                    1.0 => scale(1),
+                    0.25 => scale(5),
+                    0.25 => scale(6),
+                    0.25 => scale(5),
+                    0.25 => scale(4),
+                    0.5 => scale(3),
+                    0.5 => scale(1),
                 ));
             });
         });
@@ -281,9 +281,9 @@ mod tests {
             score.new_measures(|m| {
                 m[0].chord(Chord::triad(tuning!(E 4), ChordQuality::Major).unwrap());
                 m[1].note(beats!(dg;
-                    1.0 => scale(3),
-                    1.0 => scale(5) / 2,
-                    2.0 => scale(1),
+                    0.5 => scale(3),
+                    0.5 => scale(5) / 2,
+                    1.0 => scale(1),
                 ));
             });
         });
